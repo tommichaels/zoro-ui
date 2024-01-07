22b44c65c6045804f478e0f4fcd85acf8c79b339
Galadriel <galadriel@zoroprotocol.com>
2024-01-07 19:24:46 +0800

    fix the walletconnect and trezore wallet issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,44 @@
 /// <reference types="vitest" />
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import svgrPlugin from 'vite-plugin-svgr';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgrPlugin from "vite-plugin-svgr";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import nodePolyfills from 'rollup-plugin-polyfill-node'
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    nodePolyfills({
+      include: ["node_modules/**/*.js", new RegExp("node_modules/.vite/.*js")],
+      http: true,
+      crypto: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      assert: 'assert'
+    }
+  },
   build: {
-    outDir: 'build',
+    outDir: "build",
+    rollupOptions: {
+      plugins: [nodePolyfills({ crypto: true, http: true })]
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.tsx',
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.tsx",
     coverage: {
-      reporter: ['text', 'html', 'json-summary', 'json'],
-      exclude: ['node_modules/', 'src/setupTests.tsx'],
+      reporter: ["text", "html", "json-summary", "json"],
+      exclude: ["node_modules/", "src/setupTests.tsx"],
     },
   },
 });
