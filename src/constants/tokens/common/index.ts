f569c7f484e1eedbdb6cd9f4619c072419b44cbb
0xUncleEnzo <0xUncleEnzo@gmail.com>
2024-01-13 13:57:05 -0500

    Match token configs to testnet and mainnet deploys

--- src/constants/tokens/common/index.ts
+++ src/constants/tokens/common/index.ts
@@ -1,68 +1,59 @@
 import { Token } from 'types';
 
 import aave from 'assets/img/tokens/aave.svg';
-import ada from 'assets/img/tokens/ada.svg';
-import alpaca from 'assets/img/tokens/alpaca.svg';
-import ankr from 'assets/img/tokens/ankr.svg';
-import ankrbnb from 'assets/img/tokens/ankrBNB.svg';
-import bch from 'assets/img/tokens/bch.svg';
-import beth from 'assets/img/tokens/beth.svg';
-import bifi from 'assets/img/tokens/bifi.png';
-import bnb from 'assets/img/tokens/bnb.svg';
-import bnbx from 'assets/img/tokens/bnbx.png';
-import bsw from 'assets/img/tokens/bsw.svg';
-import btcb from 'assets/img/tokens/btcb.svg';
-import btt from 'assets/img/tokens/btt.svg';
-import busd from 'assets/img/tokens/busd.svg';
-import cake from 'assets/img/tokens/cake.svg';
 import dai from 'assets/img/tokens/dai.svg';
 import doge from 'assets/img/tokens/doge.svg';
-import dot from 'assets/img/tokens/dot.svg';
 import eth from 'assets/img/tokens/eth.svg';
-import fil from 'assets/img/tokens/fil.svg';
-import floki from 'assets/img/tokens/floki.svg';
-import hay from 'assets/img/tokens/hay.png';
 import link from 'assets/img/tokens/link.svg';
-import ltc from 'assets/img/tokens/ltc.svg';
-import luna from 'assets/img/tokens/luna.svg';
-import matic from 'assets/img/tokens/matic.svg';
-import mute from 'assets/img/tokens/mute.svg';
-import nft from 'assets/img/tokens/nft.png';
-import raca from 'assets/img/tokens/raca.png';
-import stkbnb from 'assets/img/tokens/stkBNB.svg';
-import sxp from 'assets/img/tokens/sxp.svg';
-import trx from 'assets/img/tokens/trx.svg';
-import tusd from 'assets/img/tokens/tusd.svg';
+import pepe from 'assets/img/tokens/pepe.jpeg';
+import uni from 'assets/img/tokens/uni.jpeg';
 import usdc from 'assets/img/tokens/usdc.svg';
-import usdd from 'assets/img/tokens/usdd.svg';
 import usdt from 'assets/img/tokens/usdt.svg';
-import ust from 'assets/img/tokens/ust.svg';
-import vai from 'assets/img/tokens/vai.svg';
-import vrt from 'assets/img/tokens/vrt.svg';
-import wbeth from 'assets/img/tokens/wbeth.svg';
-import wbnb from 'assets/img/tokens/wbnb.svg';
-import win from 'assets/img/tokens/win.svg';
-import xrp from 'assets/img/tokens/xrp.svg';
-import xvs from 'assets/img/tokens/xvs.svg';
+import wbtc from 'assets/img/tokens/btcb.svg';
 
 export const TOKEN_INFO = {
-  "test": {
+  "aave": {
     address: "",
     decimals: 18,
-    symbol: 'TEST',
-    asset: usdc,
+    symbol: 'AAVE',
+    asset: aave,
   } as Token,
-  "bnb": {
+  "dai": {
     address: "",
     decimals: 18,
-    symbol: 'BNB',
-    asset: bnb,
+    symbol: 'DAI',
+    asset: dai,
   } as Token,
-  "sxp": {
+  "doge": {
+    address: "",
+    decimals: 8,
+    symbol: 'DOGE',
+    asset: doge,
+  } as Token,
+  "eth": {
     address: "",
     decimals: 18,
-    symbol: 'SXP',
-    asset: sxp,
+    symbol: 'ETH',
+    asset: eth,
+    isNative: true
+  } as Token,
+  "link": {
+    address: "",
+    decimals: 18,
+    symbol: 'LINK',
+    asset: link,
+  } as Token,
+  "pepe": {
+    address: "",
+    decimals: 18,
+    symbol: 'PEPE',
+    asset: pepe,
+  } as Token,
+  "uni": {
+    address: "",
+    decimals: 18,
+    symbol: 'UNI',
+    asset: uni,
   } as Token,
   "usdc": {
     address: "",
@@ -76,258 +67,10 @@ export const TOKEN_INFO = {
     symbol: 'USDT',
     asset: usdt,
   } as Token,
-  "busd": {
-    address: "",
-    decimals: 18,
-    symbol: 'BUSD',
-    asset: busd,
-  } as Token,
-  "xvs": {
-    address: "",
-    decimals: 18,
-    symbol: 'XVS',
-    asset: xvs,
-  } as Token,
   "wbtc": {
     address: "",
     decimals: 8,
     symbol: 'WBTC',
-    asset: btcb,
-  } as Token,
-  "eth": {
-    address: "",
-    decimals: 18,
-    symbol: 'ETH',
-    asset: eth,
-    isNative: true
-  } as Token,
-  "ltc": {
-    address: "",
-    decimals: 18,
-    symbol: 'LTC',
-    asset: ltc,
-  } as Token,
-  "xrp": {
-    address: "",
-    decimals: 18,
-    symbol: 'XRP',
-    asset: xrp,
-  } as Token,
-  "bch": {
-    address: "",
-    decimals: 18,
-    symbol: 'BCH',
-    asset: bch,
-  } as Token,
-  "dot": {
-    address: "",
-    decimals: 18,
-    symbol: 'DOT',
-    asset: dot,
-  } as Token,
-  "link": {
-    address: "",
-    decimals: 18,
-    symbol: 'LINK',
-    asset: link,
-  } as Token,
-  "dai": {
-    address: "",
-    decimals: 18,
-    symbol: 'DAI',
-    asset: dai,
-  } as Token,
-  "fil": {
-    address: "",
-    decimals: 18,
-    symbol: 'FIL',
-    asset: fil,
-  } as Token,
-  "beth": {
-    address: "",
-    decimals: 18,
-    symbol: 'BETH',
-    asset: beth,
-  } as Token,
-  "ada": {
-    address: "",
-    decimals: 18,
-    symbol: 'ADA',
-    asset: ada,
-  } as Token,
-  "doge": {
-    address: "",
-    decimals: 8,
-    symbol: 'DOGE',
-    asset: doge,
-  } as Token,
-  "matic": {
-    address: "",
-    decimals: 18,
-    symbol: 'MATIC',
-    asset: matic,
-  } as Token,
-  "cake": {
-    address: "",
-    decimals: 18,
-    symbol: 'CAKE',
-    asset: cake,
-  } as Token,
-  "aave": {
-    address: "",
-    decimals: 18,
-    symbol: 'AAVE',
-    asset: aave,
-  } as Token,
-  "tusd": {
-    address: "",
-    decimals: 18,
-    symbol: 'TUSD',
-    asset: tusd,
-  } as Token,
-  "tusdold": {
-    address: "",
-    decimals: 18,
-    symbol: 'TUSDOLD',
-    asset: tusd,
-  } as Token,
-  "trx": {
-    address: "",
-    decimals: 6,
-    symbol: 'TRX',
-    asset: trx,
-  } as Token,
-  "trxold": {
-    address: "",
-    decimals: 18,
-    symbol: 'TRXOLD',
-    asset: trx,
-  } as Token,
-  "vai": {
-    address: "",
-    decimals: 18,
-    symbol: 'VAI',
-    asset: vai,
-  } as Token,
-  "vrt": {
-    address: "",
-    decimals: 18,
-    symbol: 'VRT',
-    asset: vrt,
-  } as Token,
-  "ust": {
-    address: "",
-    decimals: 6,
-    symbol: 'UST',
-    asset: ust,
-  } as Token,
-  "luna": {
-    address: "",
-    decimals: 6,
-    symbol: 'LUNA',
-    asset: luna,
-  } as Token,
-  "wbeth": {
-    address: "",
-    decimals: 18,
-    symbol: 'WBETH',
-    asset: wbeth,
-  } as Token,
-  "hay": {
-    address: "",
-    decimals: 18,
-    symbol: 'HAY',
-    asset: hay,
-  } as Token,
-  "usdd": {
-    address: "",
-    decimals: 18,
-    symbol: 'USDD',
-    asset: usdd,
-  } as Token,
-  // Underlying tokens for isolated markets
-  "mute": {
-    address: "",
-    decimals: 18,
-    symbol: 'MUTE',
-    asset: mute,
-  } as Token,
-  "btt": {
-    address: "",
-    decimals: 18,
-    symbol: 'BTT',
-    asset: btt,
-  } as Token,
-  "nft": {
-    address: "",
-    decimals: 18,
-    symbol: 'NFT',
-    asset: nft,
-  } as Token,
-  "win": {
-    address: "",
-    decimals: 18,
-    symbol: 'WIN',
-    asset: win,
-  } as Token,
-  "raca": {
-    address: "",
-    decimals: 18,
-    symbol: 'RACA',
-    asset: raca,
-  } as Token,
-  "floki": {
-    address: "",
-    decimals: 18,
-    symbol: 'FLOKI',
-    asset: floki,
-  } as Token,
-  "bifi": {
-    address: "",
-    decimals: 18,
-    symbol: 'BIFI',
-    asset: bifi,
-  } as Token,
-  "bsw": {
-    address: "",
-    decimals: 18,
-    symbol: 'BSW',
-    asset: bsw,
-  } as Token,
-  "alpaca": {
-    address: "",
-    decimals: 18,
-    symbol: 'ALPACA',
-    asset: alpaca,
-  } as Token,
-  "ankr": {
-    address: "",
-    decimals: 18,
-    symbol: 'ANKR',
-    asset: ankr,
-  } as Token,
-  "ankrbnb": {
-    address: "",
-    decimals: 18,
-    symbol: 'ankrBNB',
-    asset: ankrbnb,
-  } as Token,
-  "bnbx": {
-    address: "",
-    decimals: 18,
-    symbol: 'BNBx',
-    asset: bnbx,
-  } as Token,
-  "stkbnb": {
-    address: "",
-    decimals: 18,
-    symbol: 'stkBNB',
-    asset: stkbnb,
-  } as Token,
-  "wbnb": {
-    address: "",
-    decimals: 18,
-    symbol: 'WBNB',
-    asset: wbnb,
+    asset: wbtc,
   } as Token,
 };
--- src/constants/tokens/vBep/index.ts
+++ src/constants/tokens/vBep/index.ts
@@ -4,294 +4,95 @@ import { TOKEN_INFO } from '../common/';
 
 export const ZTOKEN_INFO = {
   // Main pool
-  'test': {
-    address: "",
-    decimals: 18,
-    symbol: 'vTEST',
-    underlyingToken: TOKEN_INFO.test,
-  } as VToken,
-  'sxp': {
+  'dai': {
     address: "",
     decimals: 8,
-    symbol: 'vSXP',
-    underlyingToken: TOKEN_INFO.sxp,
+    symbol: 'zDAI',
+    underlyingToken: TOKEN_INFO.dai,
+  } as VToken,
+  'eth': {
+    address: "",
+    decimals: 8,
+    symbol: 'zETH',
+    underlyingToken: TOKEN_INFO.eth,
   } as VToken,
   'usdc': {
     address: "",
     decimals: 8,
-    symbol: 'vUSDC',
+    symbol: 'zUSDC',
     underlyingToken: TOKEN_INFO.usdc,
   } as VToken,
   'usdt': {
     address: "",
     decimals: 8,
-    symbol: 'vUSDT',
+    symbol: 'zUSDT',
     underlyingToken: TOKEN_INFO.usdt,
-  } as VToken,
-  'busd': {
-    address: "",
-    decimals: 8,
-    symbol: 'vBUSD',
-    underlyingToken: TOKEN_INFO.busd,
-  } as VToken,
-  'bnb': {
-    address: "",
-    decimals: 8,
-    symbol: 'vBNB',
-    underlyingToken: TOKEN_INFO.bnb,
-  } as VToken,
-  'xvs': {
-    address: "",
-    decimals: 8,
-    symbol: 'vXVS',
-    underlyingToken: TOKEN_INFO.xvs,
   } as VToken,
   'wbtc': {
     address: "",
     decimals: 8,
-    symbol: 'vBTC',
+    symbol: 'zWBTC',
     underlyingToken: TOKEN_INFO.wbtc,
   } as VToken,
-  'eth': {
-    address: "",
-    decimals: 18,
-    symbol: 'vETH',
-    underlyingToken: TOKEN_INFO.eth,
-  } as VToken,
-  'ltc': {
+  // Isolated assets
+  'dege:aave': {
     address: "",
     decimals: 8,
-    symbol: 'vLTC',
-    underlyingToken: TOKEN_INFO.ltc,
-  } as VToken,
-  'xrp': {
-    address: "",
-    decimals: 8,
-    symbol: 'vXRP',
-    underlyingToken: TOKEN_INFO.xrp,
-  } as VToken,
-  'bch': {
-    address: "",
-    decimals: 8,
-    symbol: 'vBCH',
-    underlyingToken: TOKEN_INFO.bch,
-  } as VToken,
-  'dot': {
-    address: "",
-    decimals: 8,
-    symbol: 'vDOT',
-    underlyingToken: TOKEN_INFO.dot,
-  } as VToken,
-  'link': {
-    address: "",
-    decimals: 8,
-    symbol: 'vLINK',
-    underlyingToken: TOKEN_INFO.link,
-  } as VToken,
-  'dai': {
-    address: "",
-    decimals: 8,
-    symbol: 'vDAI',
-    underlyingToken: TOKEN_INFO.dai,
-  } as VToken,
-  'fil': {
-    address: "",
-    decimals: 8,
-    symbol: 'vFIL',
-    underlyingToken: TOKEN_INFO.fil,
-  } as VToken,
-  'beth': {
-    address: "",
-    decimals: 8,
-    symbol: 'vBETH',
-    underlyingToken: TOKEN_INFO.beth,
-  } as VToken,
-  'ada': {
-    address: "",
-    decimals: 8,
-    symbol: 'vADA',
-    underlyingToken: TOKEN_INFO.ada,
-  } as VToken,
-  'doge': {
-    address: "",
-    decimals: 8,
-    symbol: 'vDOGE',
-    underlyingToken: TOKEN_INFO.doge,
-  } as VToken,
-  'matic': {
-    address: "",
-    decimals: 8,
-    symbol: 'vMATIC',
-    underlyingToken: TOKEN_INFO.matic,
-  } as VToken,
-  'cake': {
-    address: "",
-    decimals: 8,
-    symbol: 'vCAKE',
-    underlyingToken: TOKEN_INFO.cake,
-  } as VToken,
-  'aave': {
-    address: "",
-    decimals: 8,
-    symbol: 'vAAVE',
+    symbol: 'zAAVE',
     underlyingToken: TOKEN_INFO.aave,
   } as VToken,
-  'tusd': {
+  'degen:dai': {
     address: "",
     decimals: 8,
-    symbol: 'vTUSD',
-    underlyingToken: TOKEN_INFO.tusd,
+    symbol: 'zDAI',
+    underlyingToken: TOKEN_INFO.dai,
   } as VToken,
-  'tusdold': {
+  'degen:doge': {
     address: "",
     decimals: 8,
-    symbol: 'vTUSDOLD',
-    underlyingToken: TOKEN_INFO.tusdold,
+    symbol: 'zDOGE',
+    underlyingToken: TOKEN_INFO.doge,
   } as VToken,
-  'trx': {
-    address: "",
-    decimals: 8,
-    symbol: 'vTRX',
-    underlyingToken: TOKEN_INFO.trx,
-  } as VToken,
-  'trxold': {
-    address: "",
-    decimals: 8,
-    symbol: 'vTRXOLD',
-    underlyingToken: TOKEN_INFO.trxold,
-  } as VToken,
-  'ust': {
-    address: "",
-    decimals: 8,
-    symbol: 'vUST',
-    underlyingToken: TOKEN_INFO.ust,
-  } as VToken,
-  'luna': {
-    address: "",
-    decimals: 8,
-    symbol: 'vLUNA',
-    underlyingToken: TOKEN_INFO.luna,
-  } as VToken,
-  'wbeth': {
-    address: "",
-    decimals: 8,
-    symbol: 'vWBETH',
-    underlyingToken: TOKEN_INFO.wbeth,
-  } as VToken,
-
-  // Isolated assets
   'degen:eth': {
     address: "",
     decimals: 8,
-    symbol: 'vETH',
+    symbol: 'zETH',
     underlyingToken: TOKEN_INFO.eth,
-  },
+  } as VToken,
+  'degen:link': {
+    address: "",
+    decimals: 8,
+    symbol: 'zLINK',
+    underlyingToken: TOKEN_INFO.link,
+  } as VToken,
+  'degen:pepe': {
+    address: "",
+    decimals: 8,
+    symbol: 'zPEPE',
+    underlyingToken: TOKEN_INFO.pepe,
+  } as VToken,
   'degen:usdc': {
     address: "",
     decimals: 8,
-    symbol: 'vUSDC',
+    symbol: 'zUSDC',
     underlyingToken: TOKEN_INFO.usdc,
-  },
-  'degen:mute': {
-    address: "",
-    decimals: 8,
-    symbol: 'vMUTE',
-    underlyingToken: TOKEN_INFO.mute,
-  },
-  // Pool 1 – Stable Coins
-  'hay': {
-    address: "",
-    decimals: 8,
-    symbol: 'vHAY',
-    underlyingToken: TOKEN_INFO.hay,
   } as VToken,
-  'usdd': {
+  'degen:usdt': {
     address: "",
     decimals: 8,
-    symbol: 'vUSDD',
-    underlyingToken: TOKEN_INFO.usdd,
+    symbol: 'zUSDT',
+    underlyingToken: TOKEN_INFO.usdt,
   } as VToken,
-  // Tron
-  'nft': {
+  'degen:uni': {
     address: "",
     decimals: 8,
-    symbol: 'vNFT',
-    underlyingToken: TOKEN_INFO.nft,
+    symbol: 'zUNI',
+    underlyingToken: TOKEN_INFO.uni,
   } as VToken,
-  'btt': {
+  'degen:wbtc': {
     address: "",
     decimals: 8,
-    symbol: 'vBTT',
-    underlyingToken: TOKEN_INFO.btt,
-  } as VToken,
-  'win': {
-    address: "",
-    decimals: 8,
-    symbol: 'vWIN',
-    underlyingToken: TOKEN_INFO.win,
-  } as VToken,
-  // GameFi
-  'raca': {
-    address: "",
-    decimals: 8,
-    symbol: 'vRACA',
-    underlyingToken: TOKEN_INFO.raca,
-  } as VToken,
-  'floki': {
-    address: "",
-    decimals: 8,
-    symbol: 'vFLOKI',
-    underlyingToken: TOKEN_INFO.floki,
-  } as VToken,
-  // DeFi
-  'alpaca': {
-    address: "",
-    decimals: 8,
-    symbol: 'vALPACA',
-    underlyingToken: TOKEN_INFO.alpaca,
-  } as VToken,
-  'ankr': {
-    address: "",
-    decimals: 8,
-    symbol: 'vANKR',
-    underlyingToken: TOKEN_INFO.ankr,
-  } as VToken,
-  'bifi': {
-    address: "",
-    decimals: 8,
-    symbol: 'vBIFI',
-    underlyingToken: TOKEN_INFO.bifi,
-  } as VToken,
-  'bsw': {
-    address: "",
-    decimals: 8,
-    symbol: 'vBSW',
-    underlyingToken: TOKEN_INFO.bsw,
-  } as VToken,
-  // Liquid Staked BNB
-  'wbnb': {
-    address: "",
-    decimals: 8,
-    symbol: 'vWBNB',
-    underlyingToken: TOKEN_INFO.wbnb,
-  } as VToken,
-  'ankrbnb': {
-    address: "",
-    decimals: 8,
-    symbol: 'vankrBNB',
-    underlyingToken: TOKEN_INFO.ankrbnb,
-  } as VToken,
-  'bnbx': {
-    address: "",
-    decimals: 8,
-    symbol: 'vBNBx',
-    underlyingToken: TOKEN_INFO.bnbx,
-  } as VToken,
-  'stkbnb': {
-    address: "",
-    decimals: 8,
-    symbol: 'vstkBNB',
-    underlyingToken: TOKEN_INFO.stkbnb,
+    symbol: 'zWBTC',
+    underlyingToken: TOKEN_INFO.wbtc,
   } as VToken,
 };
-
