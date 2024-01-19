d34bbe53c1371fd0314a69d2d9974760cb722d2f
gokalpy092 <gokalpy092@gmail.com>
2024-01-19 18:57:45 -0300

    fix Approve flow before Borrow

--- src/containers/AssetAccessor/index.tsx
+++ src/containers/AssetAccessor/index.tsx
@@ -1,22 +1,20 @@
 /** @jsxImportSource @emotion/react */
-import { ApproveToken, ConnectWallet, Spinner } from 'components'
-import React from 'react'
-import { Asset, Pool, TokenAction, VToken } from 'types'
-import { areTokensEqual, isTokenActionEnabled } from 'utilities'
-
-import { useGetPool } from 'clients/api'
-import { useAuth } from 'context/AuthContext'
-import useAssetInfo from 'hooks/useAssetInfo'
-
-import DisabledActionNotice from './DisabledActionNotice'
+import DisabledActionNotice from "./DisabledActionNotice";
+import { useGetPool } from "clients/api";
+import { ApproveToken, ConnectWallet, Spinner } from "components";
+import { useAuth } from "context/AuthContext";
+import useAssetInfo from "hooks/useAssetInfo";
+import React from "react";
+import { Asset, Pool, TokenAction, VToken } from "types";
+import { areTokensEqual, isTokenActionEnabled } from "utilities";
 
 export interface AssetAccessorProps {
-  vToken: VToken
-  poolComptrollerAddress: string
-  connectWalletMessage: string
-  approveTokenMessage: string
-  action: TokenAction
-  children: (props: { asset: Asset; pool: Pool }) => React.ReactNode
+  vToken: VToken;
+  poolComptrollerAddress: string;
+  connectWalletMessage: string;
+  approveTokenMessage: string;
+  action: TokenAction;
+  children: (props: { asset: Asset; pool: Pool }) => React.ReactNode;
 }
 
 const AssetAccessor: React.FC<AssetAccessorProps> = ({
@@ -25,50 +23,55 @@ const AssetAccessor: React.FC<AssetAccessorProps> = ({
   children,
   connectWalletMessage,
   approveTokenMessage,
-  action
+  action,
 }) => {
-  const { accountAddress } = useAuth()
+  const { accountAddress } = useAuth();
 
   const { data: getPoolData } = useGetPool({
     poolComptrollerAddress,
-    accountAddress
-  })
-  const pool = getPoolData?.pool
-  const asset = pool?.assets.find(item => areTokensEqual(item.vToken, vToken))
+    accountAddress,
+  });
+  const pool = getPoolData?.pool;
+  const asset = pool?.assets.find((item) =>
+    areTokensEqual(item.vToken, vToken)
+  );
+  const type =
+    action === "supply" || action === "withdraw" ? "supply" : "borrow";
 
-  const assetInfo = useAssetInfo({
-    asset,
-    type: action === 'supply' || action === 'withdraw' ? 'supply' : 'borrow'
-  })
+  const assetInfo = useAssetInfo({ asset, type });
 
   //console.log("underlying vtoken", vToken);
   if (
     !isTokenActionEnabled({
       token: vToken.underlyingToken,
-      action
+      action,
     })
   ) {
     return (
       <DisabledActionNotice token={vToken.underlyingToken} action={action} />
-    )
+    );
   }
 
   return (
     <ConnectWallet message={connectWalletMessage}>
       {pool && asset ? (
-        <ApproveToken
-          token={vToken.underlyingToken}
-          spenderAddress={vToken.address}
-          title={approveTokenMessage}
-          assetInfo={assetInfo}
-        >
-          {children({ asset, pool })}
-        </ApproveToken>
+        type === "borrow" ? (
+          children({ asset, pool })
+        ) : (
+          <ApproveToken
+            token={vToken.underlyingToken}
+            spenderAddress={vToken.address}
+            title={approveTokenMessage}
+            assetInfo={assetInfo}
+          >
+            {children({ asset, pool })}
+          </ApproveToken>
+        )
       ) : (
         <Spinner />
       )}
     </ConnectWallet>
-  )
-}
+  );
+};
 
-export default AssetAccessor
+export default AssetAccessor;
