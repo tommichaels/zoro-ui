3da9ff94eaed7787840ceb091542c7d314909f1d
Galadriel <galadriel@zoroprotocol.com>
2024-02-21 14:46:47 -0400

    blocks per day

--- src/pages/Market/index.tsx
+++ src/pages/Market/index.tsx
@@ -21,6 +21,7 @@ import PLACEHOLDER_KEY from "constants/placeholderKey";
 import { routes } from "constants/routing";
 import { TOKENS } from "constants/tokens";
 import { BLOCKS_PER_DAY } from "constants/zk";
+import { SECONDS_PER_DAY } from "constants/zk";
 import { useAuth } from "context/AuthContext";
 import { useHideXlDownCss, useShowXlDownCss } from "hooks/responsive";
 import useOperationModal from "hooks/useOperationModal";
@@ -88,9 +89,9 @@ export const MarketUi: React.FC<MarketUiProps> = ({
       // precise
 
       // prettier-ignore
-      dailySupplyInterestsCents: asset && +asset.supplyBalanceCents * (((1 + asset.supplyRatePerBlockTokens.toNumber()) ** BLOCKS_PER_DAY) - 1),
+      dailySupplyInterestsCents: asset && +asset.supplyBalanceCents * (((1 + asset.supplyRatePerBlockTokens.toNumber()) ** SECONDS_PER_DAY) - 1),
       // prettier-ignore
-      dailyBorrowInterestsCents: asset && +asset.borrowBalanceCents * (((1 + asset.borrowRatePerBlockTokens.toNumber()) ** BLOCKS_PER_DAY) - 1),
+      dailyBorrowInterestsCents: asset && +asset.borrowBalanceCents * (((1 + asset.borrowRatePerBlockTokens.toNumber()) ** SECONDS_PER_DAY) - 1),
     }),
     [
       asset?.supplyRatePerBlockTokens,
